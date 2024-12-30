03e856ca5fd44d43d914b4d1fcaef058f69e7614
crab <imrcrab@163.com>
2024-12-30 14:37:25 +0800

    update: client config

--- src/main.rs
+++ src/main.rs
@@ -46,9 +46,11 @@ async fn create_client() -> Result<Client, Box<dyn std::error::Error + Send + Sy
         .http2_keep_alive_interval(Some((Duration::from_secs(600)))) // 设置 HTTP/2 保活时间
         .http2_keep_alive_while_idle(true)
         .http2_prior_knowledge() // 启用 HTTP/2 优化
+        .https_only(true) 
         .pool_max_idle_per_host(900) // 设置每个主机的最大空闲连接数
         .pool_idle_timeout(Some(Duration::from_secs(90))) // 设置连接池空闲超时时间
         .timeout(Duration::from_secs(10)) // 设置请求超时时间
+        // .set_tls_enable_early_data(true) // 启用 TLS 1.3 0-RTT
         .build()?;
     Ok(client)
 }
