aa57918445c66d201daf3b298e9223b14697c456
crab <imrcrab@163.com>
2024-12-30 14:59:55 +0800

    update: client config

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ async fn create_client() -> Result<Client, Box<dyn std::error::Error + Send + Sy
         .https_only(true) 
         .pool_max_idle_per_host(900) // 设置每个主机的最大空闲连接数
         .pool_idle_timeout(None) // 设置连接池空闲超时时间
-        .timeout(Duration::from_secs(10)) // 设置请求超时时间
+        // .timeout(Duration::from_secs(10)) // 设置请求超时时间
         .set_tls_enable_early_data(true) // 启用 TLS 1.3 0-RTT
         .build()?;
     Ok(client)
