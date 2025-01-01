9d933472b624ab1165f464120fc8914febf56d56
crab <imrcrab@163.com>
2025-01-01 20:38:13 +0800

    update: 512-client config

--- src/main.rs
+++ src/main.rs
@@ -212,7 +212,7 @@ async fn find_and_update(
     });
 }
 
-#[tokio::main(flavor = "multi_thread", worker_threads = 8)]
+#[tokio::main(flavor = "multi_thread", worker_threads = 512)]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let globalDashMap = create_dashmap().await?;
     let globalDashPreMap = create_dashmap().await?;
