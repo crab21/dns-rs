e30d2cd22891d01a4a96b8a7650a21fc8b0cae65
crab <imrcrab@163.com>
2025-01-04 20:44:46 +0800

    fix: query type for cache

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ struct Config {
     enable_clear_expired_cache: bool,
     resolve_skip_domains: Vec<String>,
     enable_cache: bool,
+    enable_sniffing: bool,
     map_init_capacity: u64,
     map_init_shard_amount: u64,
     ttl_range_multi: HashMap<u64, u64>,
@@ -189,6 +190,88 @@ fn parse_ip_ttl(
         query_type: types,
     };
     Ok(Arc::new(responseResult))
+}
+
+async fn find_and_update(
+    domain: String,
+    globalDashMap: Arc<DashMap<String, Arc<DOHResponse>>>,
+    client: Arc<Client>,
+    doh_urls: Vec<String>,
+    requestBody: Vec<u8>,
+    config: Config,
+) {
+    let global_dash_map_clone = Arc::clone(&globalDashMap);
+    let client_clone = client.clone();
+    let config_clone = config.clone();
+    let domain_clone = domain.clone();
+    if config_clone.enable_sniffing == false {
+        return;
+    }
+
+    tokio::spawn(async move {
+        let ttl = global_dash_map_clone
+            .get(&domain_clone)
+            .map(|v| {
+                let now = SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_secs();
+                let mut multi_num = v.ttl;
+                config.ttl_range_multi.keys().collect::<Vec<&u64>>().sort();
+                for (k, vv) in config.ttl_range_multi.iter() {
+                    if *k > v.ttl {
+                        multi_num = multi_num * (*vv);
+                        break;
+                    }
+                    continue;
+                }
+
+                let datetime_shanghai = Utc.timestamp_opt((v.ttl + v.last_update + multi_num) as i64, 0).unwrap().with_timezone(&chrono_tz::Asia::Shanghai);
+                // 格式化为字符串
+                let formatted = datetime_shanghai.format("%Y-%m-%d %H:%M:%S").to_string();
+                println!(
+                    "domain: {:?}, ttl: {:?}, now: {:?}, expire_time: {:?}",
+                    domain_clone,
+                    v.ttl,
+                    now,
+                    formatted
+                );
+                if v.ttl > 0 && now <= v.ttl + v.last_update + multi_num {
+                    return 0;
+                }
+                if (now - v.last_update) < 60 {
+                    return 0;
+                }
+                println!(
+                    "check domain {:?} need to update, ttl:{:?}, last_update:{:?}",
+                    domain_clone, v.exipre_time, v.last_update
+                );
+                v.ttl
+            })
+            .unwrap_or_else(|| 0);
+        if ttl == 0 {
+            return;
+        }
+
+        if let Ok(rr) =
+            forward_to_fastest_doh(client_clone, domain_clone, requestBody, doh_urls, config).await
+        {
+            let rcopy = rr.clone();
+            match parse_ip_ttl(rcopy.as_slice(), config_clone) {
+                Ok(resp) => {
+                    println!(
+                        "*******Caching response for domain: {:?}*******",
+                        domain.clone()
+                    );
+                    global_dash_map_clone.insert(domain, resp);
+                }
+                Err(e) => {
+                    eprintln!("Failed to parse TTL: {}", e);
+                }
+            }
+        };
+        println!("globalDashMap len is: {:?}", globalDashMap.len());
+    });
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 8)]
@@ -346,6 +429,17 @@ async fn recv_and_do_resolve(
 
                 match sendRespose {
                     Ok(_) => {
+                        if config.enable_sniffing {
+                            find_and_update(
+                                domainName.clone(),
+                                globalDashMap,
+                                client,
+                                config.dohs.clone(),
+                                buf[..len].to_vec(),
+                                config.clone(),
+                            )
+                            .await;
+                        }
                         return Ok(());
                     }
                     Err(e) => {
